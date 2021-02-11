16b9d96c1df3ce5b8a5134e37c78dd3def849bc2
SneyderHOL <1507@holbertonschool.com>
2021-02-11 17:05:42 -0500

    [UPDATE]: save user funcionality

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,4 +5,18 @@ class ProfilesController < ApplicationController
 
   def list
   end
+
+  def create
+    @user = User.new(user_param)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_param
+    params.require(:user).permit(:email, :username, :firstname, :lastname, :password, :password_confirmation)
+  end
 end
